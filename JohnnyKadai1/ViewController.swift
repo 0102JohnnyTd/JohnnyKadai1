1dce8a29d02203a7a27fa54ab1e90778755a5012
0102johnny <rt2824.recruit.1022@gmail.com>
2022-02-08 10:32:34 +0900

    ・UITextFieldにextensionで入力値をInt型に変換して取得するコンピューテッドプロパティを作成 ・@IBOutletにprivateをつける

--- JohnnyKadai1/ViewController.swift
+++ JohnnyKadai1/ViewController.swift
@@ -8,22 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var firstValueTextField: UITextField!
+    @IBOutlet weak private var firstValueTextField: UITextField!
 
-    @IBOutlet weak var secondValueTextField: UITextField!
+    @IBOutlet weak private var secondValueTextField: UITextField!
 
-    @IBOutlet weak var thirdValueTextField: UITextField!
+    @IBOutlet weak private var thirdValueTextField: UITextField!
 
-    @IBOutlet weak var fourthValueTextField: UITextField!
+    @IBOutlet weak private var fourthValueTextField: UITextField!
 
-    @IBOutlet weak var fifthValuetextField: UITextField!
+    @IBOutlet weak private var fifthValuetextField: UITextField!
 
-    @IBOutlet weak var caluclateButton: UIButton! 
+    @IBOutlet weak private var caluclateButton: UIButton! {
+        didSet {
+            caluclateButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        }
+    }
+    @objc func didTapButton() {
+        let firstValue = firstValueTextField.textToInt
+        let secondValue = secondValueTextField.textToInt
+        let thirdValue = thirdValueTextField.textToInt
+        let fourthValue = fourthValueTextField.textToInt
+        let fifthValue = fifthValuetextField.textToInt
 
-    @IBOutlet weak var resultLabel: UILabel!
+        let total = firstValue + secondValue + thirdValue + fourthValue + fifthValue
+        resultLabel.text = String(total)
+    }
+
+//    func executeCalculation() {
+//        let firstValue:Int? = Int(firstValueTextField.text ?? "")
+//        let secondValue:Int? = Int(secondValueTextField.text ?? "")
+//        let thirdValue:Int? = Int(thirdValueTextField.text ?? "")
+//        let fourthValue:Int? = Int(fourthValueTextField.text ?? "")
+//        let fifthValue:Int? = Int(fifthValuetextField.text ?? "")
+//
+//        let result = firstValue + secondValue + thirdValue + fourthValue + fifthValue
+//    }
+
+    @IBOutlet weak private var resultLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+}
+
+extension UITextField {
+    var textToInt: Int {
+        let text = self.text
+        let int = text.flatMap{Int($0)} ?? 0
+        return int
     }
 }
